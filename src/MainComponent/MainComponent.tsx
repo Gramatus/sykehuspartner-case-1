306f563261e1cf76917bcafd724f9f212be645b5
Torgeir Østtveit Størset <torgst@sykehuspartner.no>
2021-03-18 13:01:43 +0100

    Finished scaffolding

--- src/MainComponent/MainComponent.tsx
+++ src/MainComponent/MainComponent.tsx
@@ -1,5 +1,47 @@
+import {
+  Coachmark,
+  DirectionalHint,
+  IconButton,
+  TeachingBubbleContent,
+  TooltipHost,
+} from "@fluentui/react";
 import * as React from "react";
 
 export function MainComponent(): React.ReactElement {
-  return <div className="MainComponent"></div>;
+  const [showCoachmark, setShowCoachmark] = React.useState(false);
+  const targetButton = React.useRef<HTMLDivElement>(null);
+  React.useEffect(() => {
+    setTimeout(() => {
+      setShowCoachmark(true);
+    }, 1000);
+  }, []);
+  return (
+    <div className="MainComponent">
+      <TooltipHost content="Tips">
+        <div ref={targetButton} className="buttonTarget">
+          <IconButton
+            className="helpIcon"
+            iconProps={{ iconName: "AddPhone" }}
+          />
+        </div>
+      </TooltipHost>
+      {showCoachmark && (
+        <Coachmark
+          target={targetButton.current}
+          positioningContainerProps={{
+            directionalHint: DirectionalHint.bottomAutoEdge,
+            doNotLayer: false,
+          }}
+        >
+          <TeachingBubbleContent
+            headline="Hei"
+            hasCloseButton
+            onDismiss={() => setShowCoachmark(false)}
+          >
+            Husk å ta kontakt hvis du har spørsmål eller står fast!
+          </TeachingBubbleContent>
+        </Coachmark>
+      )}
+    </div>
+  );
 }
--- src/index.ts.tsx
+++ src/index.ts.tsx
@@ -1,6 +1,9 @@
+import { initializeIcons } from "@fluentui/font-icons-mdl2";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { MainComponent } from "./MainComponent/MainComponent";
+
+initializeIcons();
 
 async function startup(): Promise<void> {
   if (!document.body || document.readyState == "loading") {
